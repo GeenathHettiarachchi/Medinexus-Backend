518b610134cef4824daac7e4076f4da83a66200f
aruna-kariyawasam <arunakariyawasam27862@gmail.com>
2024-12-19 12:49:08 +0530

    Create Doctor Class

---
+++ src/main/java/com/example/Medinexus/Model/Doctor.java
@@ -0,0 +1,31 @@
+package com.example.Medinexus.Model;
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "doctors")
+@Data
+public class Doctor {
+    @Id
+    private String id;
+
+    private String fullName;
+    private String gender;
+    private LocalDate dateOfBirth;
+    private String specialization;
+    private String medicalLicenseNumber;
+    private Integer yearsOfExperience;
+    private String email;
+    private String phoneNumber;
+    private String clinicAddress;
+    private String preferredContactMethod;
+    private String username;
+    private String password;
+    private String medicalCertificationFilePath;
+    private String profilePictureFilePath;
+    private List<String> availableDays;
+    private List<String> availableTimeSlots;
+}
