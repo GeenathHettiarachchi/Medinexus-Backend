03b5b023704246af900ce5511f24121566ba46b4
geenath-hettiarachchi <dls.geenath@gmail.com>
2024-12-19 11:14:10 +0530

    first commit

---
+++ src/main/java/com/example/Medinexus/MedinexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.Medinexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedinexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedinexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Medinexus/MedinexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Medinexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedinexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
