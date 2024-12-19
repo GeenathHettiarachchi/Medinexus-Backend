4ff9c5f3b4feabc28cdb5a3f4e4a1f526a89b3c7
aruna-kariyawasam <arunakariyawasam27862@gmail.com>
2024-12-19 12:57:24 +0530

    Create DoctorController Class

---
+++ src/main/java/com/example/Medinexus/Controller/DoctorController.java
@@ -0,0 +1,51 @@
+package com.example.Medinexus.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import com.example.Medinexus.Model.Doctor;
+import com.example.Medinexus.Service.DoctorService;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorController {
+    @Autowired
+    private DoctorService doctorService;
+
+    @PostMapping
+    public ResponseEntity<Doctor> saveDoctor(@RequestBody Doctor doctor){
+        return new ResponseEntity<>(doctorService.saveDoctor(doctor), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorService.getAllDoctors();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Doctor> getDoctorById(@PathVariable String id) {
+        return new ResponseEntity<>(doctorService.getDoctorById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Doctor> updateDoctor(@PathVariable String id, @RequestBody Doctor doctor) {
+        return new ResponseEntity<>(doctorService.updateDoctor(doctor, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteDoctor(@PathVariable String id) {
+        doctorService.deleteDoctor(id);
+        return new ResponseEntity<>("Doctor deleted successfully", HttpStatus.OK);
+    }
+}
