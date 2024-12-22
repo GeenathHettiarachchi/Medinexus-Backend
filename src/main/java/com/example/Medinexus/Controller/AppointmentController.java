2ad9f193db5d872ebbb0cd9c21862fab90dc7534
geenath-hettiarachchi <dls.geenath@gmail.com>
2024-12-22 19:08:47 +0530

    created AppointmentController Class

---
+++ src/main/java/com/example/Medinexus/Controller/AppointmentController.java
@@ -0,0 +1,51 @@
+package com.example.Medinexus.Controller;
+
+import com.example.Medinexus.Model.Appointment;
+import com.example.Medinexus.Service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/appointments")
+public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @PostMapping
+    public ResponseEntity<Appointment> saveAppointment(@RequestBody Appointment appointment){ 
+        return new ResponseEntity<>(appointmentService.saveAppointment(appointment), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Appointment> getAllAppointments() {
+        return appointmentService.getAllAppointments();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Appointment> getAppointmentById(@PathVariable String id) {
+        return new ResponseEntity<>(appointmentService.getAppointmentById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Appointment> updateAppointment(@PathVariable String id, @RequestBody Appointment appointment) {
+        return new ResponseEntity<>(appointmentService.updateAppointment(appointment, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAppointment(@PathVariable String id) {
+        appointmentService.deleteAppointment(id);
+        return new ResponseEntity<>("Appointment deleted successfully", HttpStatus.OK);
+    }
+}
