6a6e905f895ac8565044e8a13ae763a40ec96e4c
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-02-02 00:22:13 +0530

    add pattern for password and email message

--- src/main/java/com/example/Medinexus/Payload/Request/SignUpRequest.java
+++ src/main/java/com/example/Medinexus/Payload/Request/SignUpRequest.java
@@ -13,13 +13,17 @@ public class SignUpRequest {
  
     @NotBlank
     @Size(max = 50)
-    @Email
+    @Email(message = "Invalid email format")
     private String email;
     
     private Set<String> roles;
     
     @NotBlank
     @Size(min = 8, max = 40)
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,40}$",
+        message = "Password must have at least one uppercase letter, one lowercase letter, one number, and one special character."
+    )
     private String password;
 
 }
