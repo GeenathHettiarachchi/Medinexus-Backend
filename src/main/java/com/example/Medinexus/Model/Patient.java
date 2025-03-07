1d636a30d205626b762466b432a48bc91bd3871e
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-03-07 07:56:34 +0530

    change patient

--- src/main/java/com/example/Medinexus/Model/Patient.java
+++ src/main/java/com/example/Medinexus/Model/Patient.java
@@ -10,19 +10,20 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Patient {
     @Id
     private String id;
+    private String userId; // Reference to the User collection
 
     private String fullName;
     private String gender;
     private String dateOfBirth;
-    private String email;
-    private String phoneNumber;
-    private String homeAddress;
     private String bloodGroup;
     private List<String> allergies;
     private List<String> existingMedicalConditions;
+    private String email;
+    private String phoneNumber;
+    private String address;
     private String emergencyContactName;
-    private String emergencyContactRelationship;
     private String emergencyContactNumber;
+    private String emergencyContactRelationship;
     private String username;
     private String password;
 }
