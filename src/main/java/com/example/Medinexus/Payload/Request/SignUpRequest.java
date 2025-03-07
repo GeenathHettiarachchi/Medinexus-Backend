265faf11d77affc57b40b9e0b66e7fa7b50d2e82
NavodPremarathna <premara-ec20010@stu.kln.ac.lk>
2025-03-07 08:09:37 +0530

    update attributes

--- src/main/java/com/example/Medinexus/Payload/Request/SignUpRequest.java
+++ src/main/java/com/example/Medinexus/Payload/Request/SignUpRequest.java
@@ -1,5 +1,7 @@
 package com.example.Medinexus.Payload.Request;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.validation.constraints.*;
@@ -22,8 +24,38 @@ public class SignUpRequest {
     @NotBlank
     @Size(min = 8, max = 40)
     @Pattern(
-        regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,40}$"
+        regexp = "^(?=.[A-Z])(?=.[a-z])(?=.\\d)(?=.[@$!%?&])[A-Za-z\\d@$!%?&]{8,40}$"
     )
     private String password;
+
+    //common fields
+    private String fullName;
+    private String gender;
+    private LocalDate dateOfBirth;
+    private String phoneNumber;
+
+    //nurse related fields
+    private String specialization;
+    private String medicalLicenseNumber;
+    private Integer yearsOfExperience;
+    private String address;
+    private String medicalCertificationFilePath;
+    private List<String> availableDays;
+
+    //doctor related fields
+    private String clinicAddress;
+    private String profilePictureFilePath;
+
+    //pharmacist related fields
+    private String pharmacyName;
+    private String drugList;
+
+    //patient related fields
+    private String bloodGroup;
+    private List<String> allergies;
+    private List<String> existingMedicalConditions;
+    private String emergencyContactName;
+    private String emergencyContactNumber;
+    private String emergencyContactRelationship;
 
 }
