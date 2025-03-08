3339e57912f1f75b85eb5e9ff27e63165fb57b51
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-03-08 15:45:16 +0530

    remove id and change the type of dateOfBirth

--- src/main/java/com/example/Medinexus/Model/Patient.java
+++ src/main/java/com/example/Medinexus/Model/Patient.java
@@ -1,20 +1,19 @@
 package com.example.Medinexus.Model;
 
 import lombok.Data;
+
+import java.time.LocalDate;
 import java.util.List;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "patients")
 @Data
 public class Patient {
-    @Id
-    private String id;
-    private String userId; // Reference to the User collection
+    private String userId;
 
     private String fullName;
     private String gender;
-    private String dateOfBirth;
+    private LocalDate dateOfBirth;
     private String bloodGroup;
     private List<String> allergies;
     private List<String> existingMedicalConditions;
