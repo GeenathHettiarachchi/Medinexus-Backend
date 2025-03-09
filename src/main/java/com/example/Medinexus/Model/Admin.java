d1ff1759c3f4c85dad8f31ff5ebe3f97ce52c6dc
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-03-09 20:37:22 +0530

    change attributes

--- src/main/java/com/example/Medinexus/Model/Admin.java
+++ src/main/java/com/example/Medinexus/Model/Admin.java
@@ -1,18 +1,16 @@
 package com.example.Medinexus.Model;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "admin")
 @Data
 public class Admin {
-
-    @Id
-    private String adminId;
+    private String userId;
     private String adminName;
     private String adminAddress;
     private String adminPhone;
-    private String adminEmail;
-    private String adminPassword;
+    private String email;
+    private String username;
+    private String password;
 }
