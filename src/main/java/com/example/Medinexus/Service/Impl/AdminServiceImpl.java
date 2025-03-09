d3384e7b20f105abeffdf028e37bd737e342db5e
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-03-10 02:38:31 +0530

    update and delete user

--- src/main/java/com/example/Medinexus/Service/Impl/AdminServiceImpl.java
+++ src/main/java/com/example/Medinexus/Service/Impl/AdminServiceImpl.java
@@ -4,16 +4,25 @@ import java.util.List;
 import java.util.Optional;
 
 import com.example.Medinexus.Model.Admin;
+import com.example.Medinexus.Model.User;
 import com.example.Medinexus.Repository.AdminRepository;
+import com.example.Medinexus.Repository.UserRepository;
 import com.example.Medinexus.Service.AdminService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminServiceImpl implements AdminService {
     @Autowired
     private AdminRepository adminRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public Admin saveAdmin(Admin admin) {
@@ -44,12 +53,25 @@ public class AdminServiceImpl implements AdminService {
         existingAdmin.setEmail(admin.getEmail());
         existingAdmin.setPassword(admin.getPassword());
         adminRepository.save(existingAdmin);
+
+        User user = userRepository.findById(existingAdmin.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Update user fields
+        user.setUsername(admin.getUsername());
+        user.setEmail(admin.getEmail());
+        if (admin.getPassword() != null && !admin.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(admin.getPassword())); // Update password
+        }
+        userRepository.save(user);
+
         return existingAdmin;
     }
 
     @Override
     public void deleteAdmin(String id) {
-        adminRepository.findById(id).orElseThrow(()-> new RuntimeException());
+        Admin admin = adminRepository.findById(id).orElseThrow(()-> new RuntimeException());
+        userRepository.deleteById(admin.getUserId());
         adminRepository.deleteById(id);
     }
 }
