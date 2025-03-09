37d76583bcf6c8c43b3691bdef6332c66b4675e2
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-03-10 02:40:59 +0530

    update and delete user

--- src/main/java/com/example/Medinexus/Service/Impl/PharmacistServiceImpl.java
+++ src/main/java/com/example/Medinexus/Service/Impl/PharmacistServiceImpl.java
@@ -4,15 +4,24 @@ import java.util.List;
 import java.util.Optional;
 
 import com.example.Medinexus.Model.Pharmacist;
+import com.example.Medinexus.Model.User;
 import com.example.Medinexus.Repository.PharmacistRepository;
+import com.example.Medinexus.Repository.UserRepository;
 import com.example.Medinexus.Service.PharmacistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PharmacistServiceImpl implements PharmacistService {
     @Autowired 
     private PharmacistRepository pharmacistRepository; 
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
  
     @Override 
     public Pharmacist savePharmacist(Pharmacist pharmacist) { 
@@ -48,12 +57,25 @@ public class PharmacistServiceImpl implements PharmacistService {
         existingPharmacist.setPharmacyName(pharmacist.getPharmacyName());
         existingPharmacist.setPassword(pharmacist.getPassword());
         pharmacistRepository.save(existingPharmacist); 
+
+        User user = userRepository.findById(existingPharmacist.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Update user fields
+        user.setUsername(pharmacist.getUsername());
+        user.setEmail(pharmacist.getEmail());
+        if (pharmacist.getPassword() != null && !pharmacist.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(pharmacist.getPassword())); // Update password
+        }
+        userRepository.save(user);
+
         return existingPharmacist; 
     } 
 
-    @Override 
-    public void deletePharmacist(String id) { 
-        pharmacistRepository.findById(id).orElseThrow(()-> new RuntimeException()); 
-        pharmacistRepository.deleteById(id); 
+    @Override
+    public void deletePharmacist(String userId) {
+        Pharmacist pharmacist = pharmacistRepository.findById(userId).orElseThrow(()-> new RuntimeException());
+        userRepository.deleteById(pharmacist.getUserId());
+        pharmacistRepository.deleteById(userId);
     }
 }
