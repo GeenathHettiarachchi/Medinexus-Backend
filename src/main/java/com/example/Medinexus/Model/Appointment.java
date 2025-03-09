42a1369f37219f279960dd1be22a5f4e42d57b2d
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-03-09 20:37:32 +0530

    add attributes

--- src/main/java/com/example/Medinexus/Model/Appointment.java
+++ src/main/java/com/example/Medinexus/Model/Appointment.java
@@ -1,5 +1,8 @@
 package com.example.Medinexus.Model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -7,7 +10,10 @@ import lombok.Data;
 @Document(collection = "appointments")
 @Data
 public class Appointment {
+
     private String doctorId;
     private String patientId;
     private String nurseId;
+    private LocalDate date;
+    private LocalTime time;
 }
