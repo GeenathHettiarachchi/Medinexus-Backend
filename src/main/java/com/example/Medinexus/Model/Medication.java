98fac15b7afc929c3f1cdda76e852265cbae20e6
geenath-hettiarachchi <dls.geenath@gmail.com>
2025-03-10 04:27:36 +0530

    add id attribute

--- src/main/java/com/example/Medinexus/Model/Medication.java
+++ src/main/java/com/example/Medinexus/Model/Medication.java
@@ -1,5 +1,6 @@
 package com.example.Medinexus.Model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -7,6 +8,10 @@ import lombok.Data;
 @Document(collection = "medications")
 @Data
 public class Medication {
+
+    @Id
+    private String id;
+    
     private String doctorId;
     private String patientId;
     private String pharmasistId;
